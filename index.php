efa0fa73c7e4e6080f95385902fe44bbdc965a45
unknown <nandasyarla58911@gmail.com>
2023-03-27 18:36:37 +0700

    Menambah navbar create customer dan product

--- index.php
+++ index.php
@@ -22,14 +22,20 @@
                 <li class="nav-item">
                 <a class="nav-link" href="product.php">product</a>
                 </li>
+                <li class="nav-item">
+                <a class="nav-link" href="crudcustomer.php">create customer</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="crudproduct.php">create product</a>
+                </li>
             </ul>
             </div>
         </div>
         </nav>
+
+
         
-        <h3>
-            <br> SELAMAT DATANG DI APLIKASI! <br>
-        </h3>
+
              
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
