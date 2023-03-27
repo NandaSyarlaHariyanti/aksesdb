09cab92e6af8079490443ffa5bd18f77c1721e77
unknown <nandasyarla58911@gmail.com>
2023-03-27 19:09:31 +0700

    Form create data product 2

--- crudproduct.php
+++ crudproduct.php
@@ -1,19 +1,20 @@
 <?php
 include('conn.php');
+$status ='';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $productCode = $_POST["productCode"];
-    $productName = $_POST["productName"];
-    $productLine = $_POST["productLine"];
-    $productScale = $_POST["productScale"];
-    $productVendor = $_POST["productVendor"];
-    $productDescription = $_POST["productDescription"];
-    $quantityInStock = $_POST["quantityInStock"];
-    $buyPrice = $_POST["buyPrice"];
-    $MSRP = $_POST["MSRP  "];
+    $productCode = $_POST['productCode'];
+    $productName = $_POST['productName'];
+    $productLine = $_POST['productLine'];
+    $productScale = $_POST['productScale'];
+    $productVendor = $_POST['productVendor'];
+    $productDescription = $_POST['productDescription'];
+    $quantityInStock = $_POST['quantityInStock'];
+    $buyPrice = $_POST['buyPrice'];
+    $MSRP = $_POST['MSRP'];
 
-    $sql = "INSERT INTO products (`productCode`,`productName`,`productLine`,`productScale`,`productVendor`,`productDescription`,`quantityInStock`,`buyPrice`,`MSRP`) VALUES ('$productCode','$productName','$productLine','$productScale','$productVendor','$productDescription','$quantityInStock','$buyPrice','$MSRP')";
+    $sql = "INSERT INTO products ( productCode , productName , productLine , productScale , productVendor , productDescription , quantityInStock , buyPrice , MSRP ) VALUES ('$productCode','$productName','$productLine','$productScale','$productVendor','$productDescription','$quantityInStock','$buyPrice','$MSRP')";
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
     } else {
@@ -60,44 +61,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 
-  
-        <label for="productCode "> Product Code :</label>
-        <input type="text" class="form-control" id="productCode " name="productCode " required><br><br>
+        <label for="productCode" class="form-label">Product Code</label>
+        <input type="text" class="form-control" name="productCode" id="ProductCode" >
 
-        <label for="productName "> Product Name :</label>
-        <input type="text" class="form-control" id="productName " name="productName " required><br><br>
+        <label for="productName" class="form-label">Product Name</label>
+        <input type="text" class="form-control" name="productName" id="productName" >
 
-        <label for="productLine">product Line</label>
+        <label for="productLine" class="form-label">Product Line</label>
         <select class="form-select" name="productLine" id="productLine">
-        <option value="classic cars"> Classic Cars</option>
-        <option value="motorcycle"> Motorcycles</option>
-        <option value="planes"> Planes</option>
-        <option value="ships"> Ships</option>
-        <option value="trains"> Trains</option>
-        <option value="trucks and buses"> Trucks and Buses</option>
-        <option value="vintage cars"> Vintage Cars</option>
+          <option selected>Choose Product Line</option>
+          <option value="classic cars">Classic Cars</option>
+          <option value="motorcycles">Motorcycles</option>
+          <option value="planes">Planes</option>
+          <option value="ships">Ships</option>
+          <option value="trains">Trains</option>
+          <option value="trucks and buses">Trucks and Buses</option>
+          <option value="vintage cars">Vintage Cars</option>
         </select>
-        <br><br>
-
-        <label for="productScale ">  product Scale:</label>
-        <input type="text" class="form-control" id="productScale " name=" productScale" required><br><br>
-
-        <label for="productVendor "> Product Vendor :</label>
-        <input type="text" class="form-control" id="productVendor " name="productVendor " required><br><br>
-
-        <label for=" productDescription"> product Description :</label>
-        <input type="text" class="form-control" id="productDescription " name=" productDescription" required><br><br>
-
-        <label for=" quantityInStock">  quantity In Stock:</label>
-        <input type="text" class="form-control" id="quantityInStock " name="quantityInStock " required><br><br>
-
-        <label for=" buyPrice"> Buy Price :</label>
-        <input type="text" class="form-control" id="buyPrice " name="buyPrice " required><br><br>
-
-        <label for=" MSRP"> MSRP :</label>
-        <input type="text" class="form-control" id="MSRP " name=" MSRP" required><br><br>
-
         
+        <label for="productScale" class="form-label">Product Scale</label>
+        <input type="text" class="form-control" name="productScale" id="productScale" >
+        
+        <label for="productVendor" class="form-label">Product Vendor</label>
+        <input type="text" class="form-control" name="productVendor" id="productVendor" >
+        
+        <label for="productDescription" class="form-label">Product Description</label>
+        <input type="text" class="form-control" name="productDescription" id="productDescription" >
+        
+        <label for="quantityInStock" class="form-label">Quantity in Stock</label>
+        <input type="text" class="form-control" name="quantityInStock" id="quantityInStock" >
+        
+        <label for="buyPrice" class="form-label">Buy Price</label>
+        <input type="text" class="form-control" name="buyPrice" id="buyPrice" >
+
+        <label for="MSRP" class="form-label">MSRP</label>
+        <input type="text" class="form-control" name="MSRP" id="MSRP" >
 
         <input type="submit" value="Create">
 
